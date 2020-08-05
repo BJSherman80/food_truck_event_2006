8450fdb37a50f1335d01fbb4655f925153f564fd
Brett Turing <bjsherman80@gmail.com>
2020-08-05 09:16:06 -0600

    added stock and inventory test

---
+++ test/food_truck_test.rb
@@ -0,0 +1,24 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+require 'pry'
+
+
+class FoodTruckTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_equal ({}), food_truck.inventory
+    assert_equal 0, food_truck.check_stock(item1)
+  end
+
+  
+
+
+
+end
